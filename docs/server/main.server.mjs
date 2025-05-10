a3d44ce62e30d7a77e0f5d0de871baff4f5d8bbf
YoussefFarahat <YoussefFarahat@live.com>
2025-05-11 00:22:42 +0300

    .

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{e,f as t,h as o,i as p,j as m}from"./chunk-QAW23V4N.mjs";import{Mb as r}from"./chunk-A4X6BB2O.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
