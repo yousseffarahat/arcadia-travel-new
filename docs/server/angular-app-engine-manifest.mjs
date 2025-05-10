a8bcc76a52292d8997194b04cb4bcac65e923c35
YoussefFarahat <YoussefFarahat@live.com>
2025-05-11 00:16:19 +0300

    add build

---
+++ docs/server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'C:/Program Files/Git/arcadia-travel-new',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
